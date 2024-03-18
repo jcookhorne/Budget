34cebe8f8938336e2e7de55f882c9a8023e7554b
Joshua Cookhorne <jncookhorne@gmail.com>
2024-03-17 22:06:33 -0400

    "changes to routing"

--- budget/src/Routing.tsx
+++ budget/src/Routing.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
 
 const Routing = () => {
+    
+    return (
+        <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" Component={Home}/>
+            </Routes>
+        </BrowserRouter>
+        </>
+    );
 
 
 }
---
+++ budget/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Home = () =>{
+    
+    
+    return(
+        <>
+            <div>
+                <h6>Hello World</h6>
+            </div>
+
+        </>
+    )
+}
+
+export default Home;
